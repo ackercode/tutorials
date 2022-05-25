97305962ae936bb525faf1fbbe6e6936ee3c3b38
alestanalves <alestanalves@gmail.com>
2022-05-25 01:50:59 -0300

    add tutorial cryptocompare

--- python-tutorials/cript_get.py
+++ python-tutorials/cript_get.py
@@ -1,9 +1,7 @@
-import cryptocompare 
+import cryptocompare
 
 btc = cryptocompare.get_price('BTC', currency='BRL')
 print(btc)
-# RESULTADO {'BTC': {'BRL': 314056.13}}
 
 doge = cryptocompare.get_price('DOGE', currency='BRL')
 print(doge)
-# RESULTADO {'DOGE': {'BRL': 1.828}}
